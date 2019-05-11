333ffedc210f6bfa9db0fb19b3569235e1026bbd
Joseph Peterson <josephpetersonn@gmail.com>
2019-05-10 21:24:21 -0500

    Dashboard work

--- src/app/components/aquarium-preview-scroller/aquarium-preview-scroller.component.ts
+++ src/app/components/aquarium-preview-scroller/aquarium-preview-scroller.component.ts
@@ -1,16 +1,34 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { SelectionModel } from '@angular/cdk/collections';
 import { MatTableDataSource, MatSort, MatPaginator, MatDialog } from '@angular/material';
+import { AquariumSnapshot } from 'src/app/models/AquariumSnapshot';
 
 @Component({
   selector: 'aquarium-preview-scroller',
   templateUrl: './aquarium-preview-scroller.component.html',
   styleUrls: ['./aquarium-preview-scroller.component.scss']
 })
-
-
 export class AquariumPreviewScrollerComponent implements OnInit {
  
+  @ViewChild("scroller") scroller;
+
+  data:AquariumSnapshot[] = [
+    new AquariumSnapshot(),
+    new AquariumSnapshot(),
+    new AquariumSnapshot(),
+    new AquariumSnapshot(),
+    new AquariumSnapshot(),
+    new AquariumSnapshot(),
+    new AquariumSnapshot(),
+    new AquariumSnapshot(),
+    new AquariumSnapshot(),
+    new AquariumSnapshot(),
+    new AquariumSnapshot(),
+    new AquariumSnapshot(),
+  ];
+
+  selectedId: number = 0;
+
 
   constructor(
     public dialog: MatDialog,
@@ -19,5 +37,33 @@ export class AquariumPreviewScrollerComponent implements OnInit {
   ngOnInit() {
     
   }
+
+
+  //Deprecated
+  scrollScroller(event: WheelEvent) {
+    var scroller:HTMLDivElement = this.scroller.nativeElement;
+    scroller.scrollTop += event.deltaY;
+    event.preventDefault();
+  }
+  handleKey(event: KeyboardEvent) {
+    event.preventDefault();
+    event.key === 'ArrowUp' ? this.propagate(-1):this.propagate(1);
+  }
+
+  setSelectedId(id: number) {
+    
+    //Handle overflow
+    if(id < 0)
+      id = this.data.length-1;
+    else if(id >= this.data.length)
+      id = 0;
+    this.selectedId = id;
+  }
+  propagate(num: number) {
+    this.setSelectedId(this.selectedId + num);
+    var snapshots:HTMLDivElement[] = this.scroller.nativeElement.getElementsByClassName("content");
+    snapshots[this.selectedId].scrollIntoView({behavior: "auto", block: "end", inline: "nearest"});
+  }
+
 }
 
---
+++ src/app/models/AquariumParameters.ts
@@ -0,0 +1,7 @@
+export class AquariumParameters
+{
+    temperature: number = 70;
+    ph: number = 7.5;
+    nitrate: number = 1;
+    nitrite: number = 2;
+}
