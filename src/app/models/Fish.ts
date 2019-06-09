b7cbcbc09ca70a5e606f5a79db4310ff751cc963
Joseph Peterson <josephpetersonn@gmail.com>
2019-06-08 21:05:06 -0500

    Fish Introduction

---
+++ src/app/models/Fish.ts
@@ -0,0 +1,9 @@
+export class Fish
+{
+  id: number //= Math.floor(Math.random() * 100);
+  aquariumId: number
+  speciesId: number
+  name: string
+  description: string
+  date: Date
+}
---
+++ src/app/models/Species.ts
@@ -0,0 +1,8 @@
+export class Species
+{
+  id: number //= Math.floor(Math.random() * 100);
+  name: string
+  description: string
+  resourceUrl: string
+  type: string
+}
